fde139af1b9caa6c49c9e3171ba8b9a92523468d
danextrem <danpeter10@hotmail.com>
2023-11-02 13:46:40 -0500

    Update Botstrap

---
+++ src/build.js
@@ -0,0 +1,7 @@
+const showMenu = document.getElementById('showMenu');
+const favDialog = document.getElementById('favDialog');
+/*const number = document.getElementById('number');*/
+
+showMenu.addEventListener('click', () => {  
+  favDialog.showModal();
+});
