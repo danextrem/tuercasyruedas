61658ff627a963e930ac4f83cc9acac1cd029afb
danextrem <danpeter10@hotmail.com>
2023-11-19 20:05:24 -0500

    update

--- src/build.js
+++ src/build.js
@@ -124,3 +124,17 @@ function clearDropDownOptions(dropDownElement, name) {
     dropDownElement.innerHTML = '<option value="" selected>'+ name +'</option>';
 }
 
+function beforeUnloadListener(event) {
+  event.preventDefault();
+  return event.returnValue = 'Are you sure you want to exit?';
+};
+
+// A function that invokes a callback when the page has unsaved changes.
+onPageHasUnsavedChanges(() => {
+  window.addEventListener('beforeunload', beforeUnloadListener);
+});
+
+// A function that invokes a callback when the page's unsaved changes are resolved.
+onAllChangesSaved(() => {
+  window.removeEventListener('beforeunload', beforeUnloadListener);
+});
