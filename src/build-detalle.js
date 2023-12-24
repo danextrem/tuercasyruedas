1556a93a4344e1e5c7d41171643ea31929a08bab
danextrem <danpeter10@hotmail.com>
2023-12-24 08:50:29 -0500

    Update

--- src/build-detalle.js
+++ src/build-detalle.js
@@ -2,5 +2,6 @@
   const navMenu = document.querySelector(".nav-menu");
   navMenu.classList.toggle("nav-menu-show");
 }
-// Asigna el evento de clic a la función
 document.querySelector("#nav-toggle").addEventListener("click", toggleNavMenu);
+
+
